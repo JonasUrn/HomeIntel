1b4946343ae8a8e7939629a3f2487a5757a39775
CaptainCutlet <jonasurnezius@gmail.com>
2025-03-03 19:13:22 +0200

    Added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
 
-import Body from './Body/Body';
+import Body from './Body/Body.js';
+import Footer from './Footer/Footer';
 
 function App() {
 
   return (
     <div className="App">
       <Body />
+      <Footer />
     </div>
   );
 }
