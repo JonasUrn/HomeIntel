830f4e0557052dca4a24966b8470263c86721f80
Ern T <ernesttr2003@gmail.com>
2025-03-06 09:12:51 +0200

    Search button triggers correctly. Also made a responsive

--- frontend/src/searchBar/searchBar.js
+++ frontend/src/searchBar/searchBar.js
@@ -2,29 +2,25 @@ import styles from "./searchBar.module.css";
 import {FontAwesomeIcon, faArrowRight} from "../imports/importFiles.js";
 import { useState, useEffect } from "react";
 
-const statistics_data = {
-    1: ["Properties evaluated", "4,114"],
-    2: ["Visirors today", "16"],
-    3: ["Happy customers", "1381"],
-    4: ["Random text", "26"]
-}
-
 const ShowForm = () => {
     useEffect(() => {
         alert("This is form");
     }, []);
-    return <div className="form">This is random text</div>
+    return <div className="form"></div>
 }
 const SearchBar = () => {
     const [clicked, setClick] = useState(false);
 
+    const handleModalForm = () => {
+        setClick(!clicked);
+    }
     return (
         <div className={styles.searchBarStatistics}>
             <div className={styles.searchDiv}>
                 <div className={styles.searchBoxText}>Begin the evaluation of your home here</div>
                 <div className={styles.searchBarDiv}>
                     <input type="text" placeholder="Enter property listing URL" className={styles.inputField}></input>
-                    <button className={styles.searchButton} onClick={() => {setClick(!clicked)}}>
+                    <button className={styles.searchButton} onClick={handleModalForm}>
                         <FontAwesomeIcon icon={faArrowRight} className={styles.arrow}/>
                     </button>
                 </div>
