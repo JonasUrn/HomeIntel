e83d83f9fbc2833cc8cc2de87af7232732324de9
Aronas Butkevičius <maronas83@gmail.com>
2025-04-05 15:23:16 +0300

    Fixed for security

--- backend/src/main/java/com/Eval/EvalScore.java
+++ backend/src/main/java/com/Eval/EvalScore.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class EvalScore {
-    private static final String API_KEY = "AIzaSyANXyIMHs85zPRiP47KG7pwmMAcjSunuSs"; // API key
+    private static final String API_KEY = "INSERT_API_HERE"; // API key
     private static final String GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY;
     private final OkHttpClient client = new OkHttpClient();
     private final Gson gson = new Gson();
